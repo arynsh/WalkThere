f784b2bc8acdc235a40cff7e18c96a2f29a39f7a
Nina Potrebich <32409475+potrebichka@users.noreply.github.com>
2019-12-03 10:03:31 -0800

    add YELP api (#4)

--- src/googleMaps.js
+++ src/googleMaps.js
@@ -1,17 +1,29 @@
+
 import loadGoogleMapsApi from 'load-google-maps-api';
-const API_KEY_MAP = 'AIzaSyB-Mgt8OR12z9WUmehS8HIQoCq_ItoMV_U';
+let API_KEY_MAP = process.env.API_KEY_MAP;
+API_KEY_MAP = 'AIzaSyB-Mgt8OR12z9WUmehS8HIQoCq_ItoMV_U';
 
 export class Map {
 
     async getMap(latitude, longitude) {
         loadGoogleMapsApi({'key': API_KEY_MAP}).then(function (googleMaps) {
-            new googleMaps.Map(document.querySelector('.map'), {
+            let currentMap = new googleMaps.Map(document.querySelector('.map'), {
                 center: {
                     lat: latitude,
                     lng: longitude
                 },
                 zoom: 12
             });
+
+            let marker = new googleMaps.Marker({
+                position: {
+                    lat: latitude,
+                    lng: longitude
+                },
+                title: "Center of Map"
+            })
+            marker.setMap(currentMap);
         });
+
     }
 }
